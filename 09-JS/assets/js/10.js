fb5eacb8bee7b187fcbb7fd23e227033079d2e99
Carole Ambert <carole.ambert@lepoles.com>
2017-04-25 16:45:47 +0200

    Add files via upload

---
+++ 09-JS/assets/js/10.js
@@ -0,0 +1,143 @@
+/* -- CONSIGNE :
+
+I. Créer un Tableau 3D "PremierTrimestre" contenant la moyenne d'un étudiant pour plusieurs matières.
+
+Nous aurons donc pour un étudiant, sa moyenne à plusieurs matières.
+
+Par exemple : Hugo LIEGEARD : [Francais : 12, Math : 19, Physique : 4], ...ect
+
+Vous allez créez au minimum 5 étudiants.
+
+II. Afficher sur la page (à l'aide de document.write) pour chaque étudiant, la liste (ul et li) de sa moyenne à chaque matière, puis sa moyenne générale.
+
+-- */
+
+
+// Petite fonction de racourci (lesflemmards.js)
+
+function w(t){
+    document.write(t);
+}
+function l(e){
+    console.log(e);
+}
+
+// -- Création de mon Tableau 3D en JS [tableau] {objet}
+
+var PremierTrimestre = [
+    {
+        "nom"     : "LIEGEARD",
+        "prenom"  : "Hugo",
+        "moyenne" : {
+            "francais" : 4,
+            "math"     : 8,
+            "physique" : 18
+        }
+        
+    },
+    {
+        "nom"     : "IHADADENE",
+        "prenom"  : "Karim",
+        "moyenne" : {
+            "francais" : 8,
+            "math"     : 18.5,
+            "physique" : 18
+        }
+    },
+    {
+        "nom"     : "HERICOURT",
+        "prenom"  : "Rudy",
+        "moyenne" : {
+            "francais" : 10.5,
+            "math"     : 8,
+            "physique" : 18
+        }
+    }
+]
+
+// -- Apercu dans la console
+l(PremierTrimestre);
+
+w('<ol>')
+// -- Je veux afficher la liste des étudiants
+for(i=0; i < PremierTrimestre.length; i++){
+    
+ 
+    
+    // -- on récupère l'objet Etudiant de l'itération
+    var Etudiant = PremierTrimestre[i];
+    
+    // -- Aperçu dans la console.
+    l(Etudiant);    
+    
+    var NombreDeMatiere = 0, SommeDesNotes = 0;
+    
+    //-- Afficher nom & prenom d'un étudiant [i] part de 0 et fait toute la liste
+//    var Etudiant = PremierTrimestre[i]; [j] renvoi les indices de la liste moyenne
+    w("<li>")
+    w(Etudiant.prenom + " " + Etudiant.nom);
+//    for( var j in Etudiant[i].moyenne) ou
+    w("<ul>")
+      for(var matiere in Etudiant.moyenne) {
+//        1(Etudiant.moyenne[matiere]);
+          NombreDeMatiere++;
+          SommeDesNotes += Etudiant.moyenne[matiere];
+          
+          w("<li>")
+            w(matiere + " : " + Etudiant.moyenne[matiere]);
+          w("</li>")
+      } // -- Fin de la boucle matière pour avoir toutes les matières
+    
+        // -- Affichage de la moyenne
+    w("<li>")
+        w("<strong>Moyenne Générale :</strong> " + (Math.round(SommeDesNotes / NombreDeMatiere)));
+    w("</li>")
+    
+    w("</ul>")
+//    for(var j in PremierTrimestre[i].moyenne){
+//        l(j);
+//    }
+    w("</li>")
+//    ou
+//    w(PremierTrimestre[i].prenom + " " + PremierTrimestre[1].nom);
+
+w('</ol>')
+}
+
+
+
+
+
+
+//function moyenne() {
+//  //renvoie la moyenne des nombres fournis en paramètre
+//  var somme=0.0;
+//  var nombre=arguments.length;
+//  for (var i=0; i<nombre; i++) somme+=arguments[i];
+//  return somme/nombre;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 09-JS/assets/js/11.js
@@ -0,0 +1,38 @@
+/* -- 
+Consigne : A partir du tableau fourni, vous devez mettre en place un système d'authentification. Après avoir demandé à votre utilisateur son EMAIL et MOT DE PASSE, et après avoir vérifié ses informations, vous lui souhaiterez la bienvenue avec son nom et prénom (document.write);
+
+En cas d'échec, vous afficherez une ALERT pour l'informer de l'erreur.
+Vous devrez préciser s'il s'agit d'une erreur au niveau du mail ou du mot de passe.
+-- */
+
+// Petite fonction de racourci (lesflemmards.js)
+
+function w(t){
+    document.write(t);
+}
+function l(e){
+    console.log(e);
+}
+
+var BaseDeDonnées = [
+    {'prenom':'Hugo','nom':'LIEGEARD','email':'wf3@hl-media.fr','mdp':'wf3'},
+    {'prenom':'Rodrigue','nom':'NOUEL','email':'rodrigue@hl-media.fr','mdp':'wf3'},
+    {'prenom':'Jean-Christophe','nom':'MONPLAISIR','email':'jc.monplaisir@hl-media.fr','mdp':'wf3'},
+    {'prenom':'Nathanael','nom':'DORDONNE','email':'nathanael.d@hl-media.fr','mdp':'wf3'}
+];
+
+
+
+// 1 -- Demander son Email à l'utilisateur avec un prompt
+var email, mdp;
+
+    var emailLogin = prompt("Veuillez entrer votre email !", "Email");
+        
+    var mdpLogin = prompt( "Veuillez entrer le mot de passe pour accéder à cette page", "Mot de passe" );
+
+// 2 -- Je vérifie si l'email saisi (emailLogin) correspond à celui en base de donnée (email)
+    if(emailLogin == email && mdpLogin == mdp) {
+        w("Bienvenue" + nom + prenom);
+} else {
+       alert("Mot de passe ou identifiant incorrect!");
+}
