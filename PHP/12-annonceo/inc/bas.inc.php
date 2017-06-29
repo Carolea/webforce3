691ca13911b029dec21429ac4410a3ea6536ecf8
Carole Ambert <carole.ambert@lepoles.com>
2017-06-29 16:59:46 +0200

    Add files via upload

---
+++ PHP/12-annonceo/inc/bas.inc.php
@@ -0,0 +1,24 @@
+ </div>
+    <!-- /.container -->
+
+    <div class="container">
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Annonceo - 2017</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+    
+
+</body>
+
+</html>
+    
---
+++ PHP/12-annonceo/inc/haut.inc.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <title>Annonceo</title>
+	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="/annonceo/sublevel/inc/css/style.css">
+	
+</head>
+<body>
+    
+     <div class="container">
+    <nav class="navbar navbar-default">
+
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">Annonceo</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li><a href="#">Qui Sommes Nous</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+          <form class="navbar-form navbar-left">
+            <div class="form-group">
+              <input type="text" onKeyPress="if(event.keyCode == 13) validerForm();" class="form-control" placeholder="Recherche...">
+            </div>
+          </form>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Espace Membre</a></li>
+          </ul>
+        </div>
+    </nav>
+  </div><!-- /.container -->
+  
+  
+  <!-- Page Content -->
+    <div class="container" style="min-height: 80vh;">
+    
